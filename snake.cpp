6829bccf9e1cbeff798400011a0d7c838abb5d69
Ozyvictus <aniketnandy784@gmail.com>
2024-01-03 14:03:10 +0530

    Snake body and movement implemented

--- snake.cpp
+++ snake.cpp
@@ -2,11 +2,25 @@
 #include <stdio.h>
 #include <conio.h>
 #include <iostream>
-// #include <unistd.h>
+#include <windows.h>
 using namespace std;
 
 int row = 20 / 2;
 int column = 40 / 2;
+int rows[10] = {row, row, row, row, row, row, row, row, row, row};
+int cols[10] = {column, 19, 18, 17, 16, 15, 14, 13, 12, 11};
+bool body = false;
+int decoy = 2409;
+enum Direction
+{
+    STOP = 0,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+};
+
+Direction dir = RIGHT;
 
 void draw()
 {
@@ -29,7 +43,23 @@ void draw()
             }
             else
             {
-                cout << " ";
+                for (int k = 1; k < sizeof(rows) / sizeof(int); k++)
+                {
+                    if (i == rows[k] && j == cols[k])
+                    {
+                        cout << "0";
+                        body = true;
+                    }
+                }
+                if (body)
+                {
+                    body = false;
+                    continue;
+                }
+                else
+                {
+                    cout << " ";
+                }
             }
         }
         cout << endl;
@@ -44,35 +74,65 @@ void logic()
 {
 }
 
+void input()
+{
+    if (_kbhit())
+    {
+        switch (_getch())
+        {
+        case 'x':
+            dir = STOP;
+            break;
+        case 'w':
+            dir = UP;
+            break;
+        case 'a':
+            dir = LEFT;
+            break;
+        case 's':
+            dir = DOWN;
+            break;
+        case 'd':
+            dir = RIGHT;
+            break;
+        }
+    }
+}
+
 int main()
 {
     int i;
     while (1)
     {
-        draw();
+        Sleep(60);
         system("cls");
-        // usleep(1000);
-        if (_kbhit())
+        draw();
+        input();
+        switch (dir)
         {
-            switch (_getch())
-            {
-            case 'x':
-                return 0;
-                break;
-            case 'w':
-                row--;
-                break;
-            case 'a':
-                column--;
-                break;
-            case 's':
-                row++;
-                break;
-            case 'd':
-                column++;
-                break;
-            }
+        case STOP:
+            return 0;
+            break;
+        case UP:
+            row--;
+            break;
+        case LEFT:
+            column--;
+            break;
+        case DOWN:
+            row++;
+            break;
+        case RIGHT:
+            column++;
+            break;
         }
+        for (int i = (sizeof(rows) / 4) - 1; i >= 0; i--)
+        {
+            rows[i + 1] = rows[i];
+            cols[i + 1] = cols[i];
+        }
+        rows[0] = row;
+        cols[0] = column;
     }
     return 0;
 }
